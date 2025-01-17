70e72379a0eb0dc9d5ecdfbf2edfbe58985f6f83
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2025-01-18 01:38:08 +0800

    update git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,8 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+alias gloga="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
 alias gp='git push origin HEAD'
 alias 'gp!'='git push origin HEAD --force-with-lease'
 
@@ -20,6 +21,7 @@ alias 'gc!'='git commit --amend --no-verify'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
+alias ga='git add'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias ge='git-edit-new'
