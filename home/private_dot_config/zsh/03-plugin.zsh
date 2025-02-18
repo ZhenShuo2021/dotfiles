ea3d195fcb341e13c51d9a08f9eebac75aa1db86
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-19 03:21:16 +0800

    use zinit

--- home/private_dot_config/zsh/03-plugin.zsh
+++ home/private_dot_config/zsh/03-plugin.zsh
@@ -1,37 +1,45 @@
-# Preload anything we need for the rest of the rc scripts
-# =============================================================================
+# Install plugin manager and source it
+### Added by Zinit's installer
 
-# Enable the function of p10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Install plugin manager and source it
-if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
-  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
 fi
-source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+# zinit light-mode for \
+#     zdharma-continuum/zinit-annex-readurl \
+#     zdharma-continuum/zinit-annex-bin-gem-node \
+#     zdharma-continuum/zinit-annex-patch-dl \
+#     zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
 
 # Load lazy-loading function to make it usable everywhere
-zcomet load romkatv/powerlevel10k
+zinit ice depth=1; zinit light romkatv/powerlevel10k
 source $ZZ/04-p10k.zsh
 
-zcomet load romkatv/zsh-defer
+zinit light romkatv/zsh-defer
 
-zsh-defer zcomet load zsh-users/zsh-autosuggestions
-zsh-defer zcomet load zsh-users/zsh-completions
-zsh-defer zcomet load zsh-users/zsh-syntax-highlighting
-zsh-defer zcomet load agkozak/zsh-z
+zsh-defer zinit light zsh-users/zsh-autosuggestions
+zsh-defer zinit light zsh-users/zsh-completions
+zsh-defer zinit light zsh-users/zsh-syntax-highlighting
+zsh-defer zinit light agkozak/zsh-z
 
 docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
-zsh-defer zcomet snippet $docker_comp
-# zsh-defer zcomet load ohmyzsh plugins/docker-compose
-zsh-defer zcomet load ohmyzsh plugins/extract
-zsh-defer zcomet load ohmyzsh plugins/git
-zcomet load ohmyzsh lib key-bindings.zsh
-
-# optional plugins
-# zsh-defer zcomet load lukechilds/zsh-better-npm-completion
-
-# deprecated plugins
-# zsh-defer zcomet load ohmyzsh lib completion.zsh   # brew fpath and zsh-completions are better
+zsh-defer zinit snippet $docker_comp
+zsh-defer zinit snippet OMZP::extract
+zsh-defer zinit snippet OMZP::git
+zsh-defer zinit snippet OMZL::key-bindings.zsh
