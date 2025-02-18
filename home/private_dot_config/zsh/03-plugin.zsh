89540190511055c417180aa00b5978206ab2b7d0
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-19 01:17:11 +0800

    use chezmoi and zcomet

--- home/private_dot_config/zsh/03-plugin.zsh
+++ home/private_dot_config/zsh/03-plugin.zsh
@@ -1,37 +1,35 @@
 # Preload anything we need for the rest of the rc scripts
 # =============================================================================
 
+# Install OMZ and plugins
+___docker_FILE="/Users/leo/.oh-my-zsh/custom/_docker"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+# git clone https://github.com/romkatv/zsh-defer  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-defer
+# docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
+# if [ ! -f "$___docker_FILE" ]; then
+#     curl -fsSL "$docker_comp" -o "$___docker_FILE"
+# fi
+
 # Enable the function of p10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Install plugin manager and source it
-if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
-  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
-fi
-source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
-
-# Load lazy-loading function to make it usable everywhere
-zcomet load romkatv/powerlevel10k
+PLUGIN_DIR=/Users/leo/.oh-my-zsh/custom/plugins
+source $PLUGIN_DIR/../themes/powerlevel10k/powerlevel10k.zsh-theme
 source $ZZ/04-p10k.zsh
+source $PLUGIN_DIR/zsh-defer/zsh-defer.plugin.zsh
 
-zcomet load romkatv/zsh-defer
+zsh-defer source $___docker_FILE
+zsh-defer source $PLUGIN_DIR/../../plugins/git/git.plugin.zsh
+zsh-defer source $PLUGIN_DIR/../../plugins/extract/extract.plugin.zsh
+zsh-defer source $PLUGIN_DIR/../../lib/key-bindings.zsh
 
-docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
-zsh-defer zcomet snippet $docker_comp
-# zsh-defer zcomet load ohmyzsh plugins/docker-compose
-zsh-defer zcomet load ohmyzsh plugins/extract
-zsh-defer zcomet load ohmyzsh plugins/git
-zcomet load ohmyzsh lib key-bindings.zsh
-
-zsh-defer zcomet load zsh-users/zsh-autosuggestions
-zsh-defer zcomet load zsh-users/zsh-completions
-zsh-defer zcomet load zsh-users/zsh-syntax-highlighting
-zsh-defer zcomet load agkozak/zsh-z
-
-# optional plugins
-# zsh-defer zcomet load lukechilds/zsh-better-npm-completion
-
-# deprecated plugins
-# zsh-defer zcomet load ohmyzsh lib completion.zsh   # brew fpath and zsh-completions are better
+zsh-defer source $PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+zsh-defer source $PLUGIN_DIR/zsh-completions/zsh-completions.plugin.zsh
+zsh-defer source $PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+zsh-defer source $PLUGIN_DIR/zsh-z/zsh-z.plugin.zsh
