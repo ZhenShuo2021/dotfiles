53bb9f94736a6b8143c11f35e2e5e1221d2b2b9c
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-19 08:31:48 +0800

    use zimfw

--- home/private_dot_config/zsh/03-plugin.zsh
+++ home/private_dot_config/zsh/03-plugin.zsh
@@ -1,37 +1,46 @@
 # Preload anything we need for the rest of the rc scripts
 # =============================================================================
 
+# setup zimfw dir
+ZIM_CONFIG_FILE=~/.config/zsh/zimrc
+ZIM_HOME=~/.config/zsh/zim
+
 # Enable the function of p10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Install plugin manager and source it
-if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
-  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+# Install zimfw if not installed
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 fi
-source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
-# Load lazy-loading function to make it usable everywhere
-zcomet load romkatv/powerlevel10k
-source $ZZ/04-p10k.zsh
+# Install p10k if not installed
+if [[ ! -d ${ZIM_HOME}/modules/powerlevel10k ]]; then
+  git clone --depth=10 https://github.com/romkatv/powerlevel10k ${ZIM_HOME}/modules/powerlevel10k
+fi
 
-zcomet load romkatv/zsh-defer
+# Install docker completion
+docker_completion="$HOME/.config/zsh/zim/modules/_docker"
+if [ ! -f "$docker_completion" ]; then
+  docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
+  curl -fsSL "$docker_comp" -o "$docker_completion"
+  unset docker_comp
+  unset docker_completion
+fi
 
-docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
-zsh-defer zcomet snippet $docker_comp
-# zsh-defer zcomet load ohmyzsh plugins/docker-compose
-zsh-defer zcomet load ohmyzsh plugins/extract
-zsh-defer zcomet load ohmyzsh plugins/git
-zcomet load ohmyzsh lib key-bindings.zsh
+# load zsh-defer and p10k
+source ${ZIM_HOME}/modules/zsh-defer/zsh-defer.plugin.zsh
+source ${ZIM_HOME}/modules/powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f ~/.config/zsh/04-p10k.zsh ]] || source ~/.config/zsh/04-p10k.zsh
 
-zsh-defer zcomet load zsh-users/zsh-autosuggestions
-zsh-defer zcomet load zsh-users/zsh-completions
-zsh-defer zcomet load zsh-users/zsh-syntax-highlighting
-zsh-defer zcomet load agkozak/zsh-z
+# load other plugins
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
+  zsh-defer source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+zsh-defer source ${ZIM_HOME}/init.zsh
+zsh-defer source $docker_completion
 
-# optional plugins
-# zsh-defer zcomet load lukechilds/zsh-better-npm-completion
-
-# deprecated plugins
-# zsh-defer zcomet load ohmyzsh lib completion.zsh   # brew fpath and zsh-completions are better
+# source again due to the bug in zimfw
+source $ZIM_HOME/modules/ohmyzsh/lib/key-bindings.zsh
