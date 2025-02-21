b41dafa4d415cf9df2950e01bed84cfd30536deb
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-21 21:46:53 +0800

    use zcomet

--- home/private_dot_config/zsh/01-plugin.zsh
+++ home/private_dot_config/zsh/01-plugin.zsh
@@ -1,58 +1,35 @@
 # Preload anything we need for the rest of the rc scripts
 # =============================================================================
 
-# setup zimfw dir
-ZIM_CONFIG_FILE=~/.config/zsh/zimrc
-ZIM_HOME=~/.config/zsh/zim
-
 # Enable the function of p10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Install zimfw if not installed
-if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+# Install plugin manager and source it
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
 fi
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
-# Install p10k if not installed
-if [[ ! -d ${ZIM_HOME}/modules/powerlevel10k ]]; then
-  git clone --depth=10 https://github.com/romkatv/powerlevel10k ${ZIM_HOME}/modules/powerlevel10k
-fi
+# load p10k
+zcomet load romkatv/zsh-defer
+zcomet load romkatv/powerlevel10k
+source $ZZ/03-p10k.zsh
 
-# Install zsh-defer if not installed
-if [[ ! -d ${ZIM_HOME}/modules/zsh-defer ]]; then
-  git clone --depth=10 https://github.com/romkatv/zsh-defer.git ${ZIM_HOME}/modules/zsh-defer
-fi
+zsh-defer zcomet load ohmyzsh plugins/extract
+zsh-defer zcomet load ohmyzsh plugins/git
+zsh-defer zcomet load ohmyzsh lib key-bindings.zsh
 
-# Install docker completion
-docker_completion="$HOME/.config/zsh/zim/modules/_docker"
-if [ ! -f "$docker_completion" ]; then
-  docker_comp="https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/zsh/_docker"
-  curl -fsSL "$docker_comp" -o "$docker_completion"
-  unset docker_comp
-  unset docker_completion
-fi
+zsh-defer zcomet load agkozak/zsh-z
+zsh-defer zcomet load zsh-users/zsh-autosuggestions
+zsh-defer zcomet load zsh-users/zsh-completions
+zsh-defer zcomet load zsh-users/zsh-syntax-highlighting
 
-# load zsh-defer and p10k
-source ${ZIM_HOME}/modules/zsh-defer/zsh-defer.plugin.zsh
-source ${ZIM_HOME}/modules/powerlevel10k/powerlevel10k.zsh-theme
-[[ ! -f ~/.config/zsh/03-p10k.zsh ]] || source ~/.config/zsh/03-p10k.zsh
+# zsh-defer zcomet compinit
 
-# change zcompdump destination
-# https://github.com/zimfw/zimfw/issues/343#issuecomment-584351654
-zsh-defer zstyle -s ':zim:completion' dumpfile 'zdumpfile' || zdumpfile=$XDG_CACHE_HOME/zsh/zcompcache/.zcompdump
-zsh-defer autoload -Uz zrecompile && zsh-defer zrecompile -p -q ${zdumpfile}
-zsh-defer unset zdumpfile
+# optional plugins
+# zcomet load lukechilds/zsh-better-npm-completion
 
-# load other plugins
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
-  zsh-defer source ${ZIM_HOME}/zimfw.zsh init -q
-fi
-
-zsh-defer source ${ZIM_HOME}/init.zsh
-zsh-defer source $docker_completion
-
-# source again due to the bug in zimfw
-source $ZIM_HOME/modules/ohmyzsh/lib/key-bindings.zsh
+# deprecated plugins
+# zcomet load ohmyzsh lib completion.zsh   # brew fpath and zsh-completions are better
--- home/private_dot_config/zsh/05-completion.zsh
+++ home/private_dot_config/zsh/05-completion.zsh
@@ -3,14 +3,17 @@
 # 請注意自動補全的載入順序必須是 FPATH > compinit > zstyle，如果插件需要
 # compdef/compinit、修改 FPATH 等等依賴，就需要手動調整順序在 completion 前載入
 
-# This MUST run before compinit
+# This MUST run before compinit. If `zcomet compinit` is called
 eval $(/opt/homebrew/bin/brew shellenv)
 
 # According to the zimfw documentation, we shouldn't run `compinit` again if `zmodule completion`  
-# is used. However, removing these two lines would require placing `eval $(/opt/homebrew/bin/brew shellenv)`  
-# before sourcing `zmodule completion`. Additionally, `zmodule` enforces execution in non-blocking mode,  
-# significantly slowing down shell startup.  
-# The current solution leverages `zsh-defer` to suppress all messages, eliminating any warnings.  
+# is used, And this is same as `zcomet compinit` However, removing these two lines would require 
+# placing `eval $(/opt/homebrew/bin/brew shellenv)` before sourcing `zmodule completion`. 
+# Additionally, `zmodule` enforces execution in non-blocking mode, significantly slowing down shell startup.  
+
+# Currently, I don't use `<plugin-manager> compinit` and leverages `zsh-defer` to delay all initializations.
+# If `<plugin-manager> compinit` is called, we can run compinit here again to trigger none-activated
+# completions and doesn't require to move the `eval $(/opt/homebrew/bin/brew shellenv)`.
 
 autoload -Uz compinit
 compinit -d "$ZSH_COMPDUMP"   # ZSH_COMPDUMP is configured in .zshenv
@@ -26,5 +29,5 @@ zstyle ':completion::complete:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
 #   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 # fi
 
-# uv (python)
+# uv (python), must after compinit
 eval "$(uv generate-shell-completion zsh)"
