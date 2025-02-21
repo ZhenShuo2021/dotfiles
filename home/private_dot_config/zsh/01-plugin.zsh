de576df04c3a90f08148a4044c1d7e42dfd9bf36
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-22 00:06:18 +0800

    use omz

--- home/private_dot_config/zsh/01-plugin.zsh
+++ home/private_dot_config/zsh/01-plugin.zsh
@@ -1,46 +1,22 @@
 # Preload anything we need for the rest of the rc scripts
 # =============================================================================
 
-# setup zimfw dir
-ZIM_CONFIG_FILE=~/.config/zsh/zimrc
-ZIM_HOME=~/.config/zsh/zim
+# Install OMZ and plugins
+# git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+# git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+# git clone https://github.com/romkatv/zsh-defer  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-defer
 
 # Enable the function of p10k instant prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Install zimfw if not installed
-if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-fi
-
-# Install p10k if not installed
-if [[ ! -d ${ZIM_HOME}/modules/powerlevel10k ]]; then
-  git clone --depth=10 https://github.com/romkatv/powerlevel10k ${ZIM_HOME}/modules/powerlevel10k
-fi
-
-# load zsh-defer and p10k
-source ${ZIM_HOME}/modules/zsh-defer/zsh-defer.plugin.zsh
-source ${ZIM_HOME}/modules/powerlevel10k/powerlevel10k.zsh-theme
-[[ ! -f ~/.config/zsh/03-p10k.zsh ]] || source ~/.config/zsh/03-p10k.zsh
-
-# load other plugins
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
-  zsh-defer source ${ZIM_HOME}/zimfw.zsh init -q
-fi
-zsh-defer source ${ZIM_HOME}/init.zsh
-
-
-# Only search full prefix match
-HISTORY_SUBSTRING_SEARCH_PREFIXED=1
-
-# disable 
-# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=white,bold,bg=cyan'
-# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=white,bold,bg=red'
-# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_TIMEOUT=2
-zsh-defer unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
-zsh-defer unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(extract git zsh-defer zsh-history-substring-search zsh-z zsh-completions zsh-syntax-highlighting zsh-autosuggestions)
+source $ZSH/oh-my-zsh.sh
+source $ZZ/03-p10k.zsh
