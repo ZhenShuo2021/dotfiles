3f044598cbd2dda60e77b5fc9300997a1cae3c4c
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-02-21 23:12:19 +0800

    better plugins

--- home/private_dot_config/zsh/01-plugin.zsh
+++ home/private_dot_config/zsh/01-plugin.zsh
@@ -12,24 +12,48 @@ if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
 fi
 source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
+
 # load p10k
 zcomet load romkatv/zsh-defer
 zcomet load romkatv/powerlevel10k
 source $ZZ/03-p10k.zsh
 
+
+# load plugins
 zsh-defer zcomet load ohmyzsh plugins/extract
 zsh-defer zcomet load ohmyzsh plugins/git
-zsh-defer zcomet load ohmyzsh lib key-bindings.zsh
 
+
+# configure zsh-history-substring-search
+zsh-defer zcomet load zsh-users/zsh-history-substring-search
+
+# Only search full prefix match
+HISTORY_SUBSTRING_SEARCH_PREFIXED=1
+
+# disable 
+# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=white,bold,bg=cyan'
+# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=white,bold,bg=red'
+# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_TIMEOUT=2
+zsh-defer unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND
+zsh-defer unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+
+# plugins that must be loaded last
 zsh-defer zcomet load agkozak/zsh-z
-zsh-defer zcomet load zsh-users/zsh-autosuggestions
 zsh-defer zcomet load zsh-users/zsh-completions
 zsh-defer zcomet load zsh-users/zsh-syntax-highlighting
+zsh-defer zcomet load zsh-users/zsh-autosuggestions
 
+
+# zcomet compinit doesn't help in my zshrc, see completion.zsh
 # zsh-defer zcomet compinit
+
 
 # optional plugins
 # zcomet load lukechilds/zsh-better-npm-completion
+
 
 # deprecated plugins
 # zcomet load ohmyzsh lib completion.zsh   # brew fpath and zsh-completions are better
