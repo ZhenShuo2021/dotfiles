56a25ad2a4f71f526c7b2438c8057e761790494e
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-06-09 23:36:59 +0800

    update

--- home/private_dot_config/zsh/03-system.zsh
+++ home/private_dot_config/zsh/03-system.zsh
@@ -1,57 +1,18 @@
-# opts from holman
-setopt NO_BG_NICE # don't nice background tasks
-# setopt NO_HUP # 不使用這個選項，要 NO_HUP 就乖乖用 tmux
-# setopt NO_LIST_BEEP
-# setopt LOCAL_OPTIONS # allow functions to have local options
-# setopt LOCAL_TRAPS # allow functions to have local traps
-# setopt HIST_VERIFY
-# setopt SHARE_HISTORY # 不使用這個選項因為運作不理想
-# setopt EXTENDED_HISTORY # add timestamps to history
-# setopt PROMPT_SUBST
-# setopt COMPLETE_IN_WORD
-# setopt IGNORE_EOF
-
-# setopt APPEND_HISTORY # adds history
-# setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-# setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-# setopt HIST_REDUCE_BLANKS
-
-# # don't expand aliases _before_ completion has finished
-# #   like: git comm-[tab]
-# setopt complete_aliases   # 此功能和大多數自動補全衝突
-
-setopt AUTO_LIST
-setopt AUTO_MENU
-setopt MENU_COMPLETE
-setopt INTERACTIVE_COMMENTS   # Recognize comments on the command line
-setopt NO_LIST_BEEP           # Don't beep on an ambiguous completion
-setopt NO_BEEP                # 完全關閉 beep
-unsetopt CORRECT              # 關閉 zsh 自動修正，很煩又不準
-setopt SH_WORD_SPLIT          # 改為和 Bash 相同的字串切割
-
-# History completion
-# https://wiki.zshell.dev/docs/guides/customization#history-optimization
-setopt append_history         # Allow multiple sessions to append to one Zsh command history.
-# setopt extended_history       # Show timestamp in history.
-setopt HIST_EXPIRE_DUPS_FIRST # Expire A duplicate event first when trimming history.
-setopt HIST_FIND_NO_DUPS      # Do not display a previously found event.
-setopt HIST_IGNORE_ALL_DUPS   # Remove older duplicate entries from history.
-setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_SPACE      # Do not record an Event Starting With A Space.
-setopt HIST_REDUCE_BLANKS     # Remove superfluous blanks from history items.
-setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
-setopt HIST_VERIFY            # Do not execute immediately upon history expansion.
-setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits.
-
-# bindkey '^[^[[D' backward-word
-# bindkey '^[^[[C' forward-word
-# bindkey '^[[5D' beginning-of-line
-# bindkey '^[[5C' end-of-line
-# bindkey '^[[3~' delete-char
-# bindkey '^?' backward-delete-char
+# Emacs Keybinding
+# - Ctrl-A: Move to the beginning of the line
+# - Ctrl-E: Move to the end of the line
+# - Ctrl-K: Delete from cursor to the end of the line
+# - Ctrl-U: Delete from cursor to the beginning of the line
+# - Ctrl-W: Delete the previous word
+# - Ctrl-Y: Paste the most recently deleted text
+# - Ctrl-F: Move forward one character
+# - Ctrl-B: Move backward one character
+# - Alt-F: Move forward one word
+# - Alt-B: Move backward one word
 
 
 # from ohmyzsh
+# =============================================================================
 # I'm not loading all key-bindings since there are bugs on my machine
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
 
@@ -153,3 +114,63 @@ bindkey '\C-x\C-e' edit-command-line
 
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
+
+
+# from holman
+# =============================================================================
+setopt NO_BG_NICE # don't nice background tasks
+# setopt NO_HUP # 不使用這個選項，要 NO_HUP 就乖乖用 tmux
+# setopt NO_LIST_BEEP
+# setopt LOCAL_OPTIONS # allow functions to have local options
+# setopt LOCAL_TRAPS # allow functions to have local traps
+# setopt HIST_VERIFY
+# setopt SHARE_HISTORY # 不使用這個選項因為運作不理想
+# setopt EXTENDED_HISTORY # add timestamps to history
+# setopt PROMPT_SUBST
+# setopt COMPLETE_IN_WORD
+# setopt IGNORE_EOF
+
+# setopt APPEND_HISTORY # adds history
+# setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+# setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+# setopt HIST_REDUCE_BLANKS
+
+# # don't expand aliases _before_ completion has finished
+# #   like: git comm-[tab]
+# setopt complete_aliases   # 此功能和大多數自動補全衝突
+
+
+# Custom
+# =============================================================================
+setopt AUTO_LIST
+setopt AUTO_MENU
+setopt MENU_COMPLETE
+setopt INTERACTIVE_COMMENTS   # Recognize comments on the command line
+setopt NO_LIST_BEEP           # Don't beep on an ambiguous completion
+setopt NO_BEEP                # 完全關閉 beep
+unsetopt CORRECT              # 關閉 zsh 自動修正，很煩又不準
+setopt SH_WORD_SPLIT          # 改為和 Bash 相同的字串切割
+
+# History completion
+# https://wiki.zshell.dev/docs/guides/customization#history-optimization
+setopt append_history         # Allow multiple sessions to append to one Zsh command history.
+# setopt extended_history       # Show timestamp in history.
+setopt HIST_EXPIRE_DUPS_FIRST # Expire A duplicate event first when trimming history.
+setopt HIST_FIND_NO_DUPS      # Do not display a previously found event.
+setopt HIST_IGNORE_ALL_DUPS   # Remove older duplicate entries from history.
+setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_SPACE      # Do not record an Event Starting With A Space.
+setopt HIST_REDUCE_BLANKS     # Remove superfluous blanks from history items.
+setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY            # Do not execute immediately upon history expansion.
+setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits.
+
+# bindkey '^[^[[D' backward-word
+# bindkey '^[^[[C' forward-word
+# bindkey '^[[5D' beginning-of-line
+# bindkey '^[[5C' end-of-line
+# bindkey '^[[3~' delete-char
+# bindkey '^?' backward-delete-char
+
+# Disable history-incremental-search-backward
+bindkey -r '^R'
